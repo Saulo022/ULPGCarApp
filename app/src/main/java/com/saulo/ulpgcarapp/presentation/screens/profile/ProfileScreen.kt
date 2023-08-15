4e01a02421fd1d5890c73c8a9ce533117e42c716
Saulo <saulo.nuez102@alu.ulpgc.es>
2023-08-15 17:57:49 +0100

    Diseño de la pantalla Profile

--- app/src/main/java/com/saulo/ulpgcarapp/presentation/screens/profile/ProfileScreen.kt
+++ app/src/main/java/com/saulo/ulpgcarapp/presentation/screens/profile/ProfileScreen.kt
@@ -1,13 +1,10 @@
 package com.saulo.ulpgcarapp.presentation.screens.profile
 
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import com.saulo.ulpgcarapp.presentation.components.DefaultButton
-import com.saulo.ulpgcarapp.presentation.navigation.AppScreen
+import com.saulo.ulpgcarapp.presentation.screens.profile.components.ProfileContent
 
 @Composable
 fun ProfileScreen(
@@ -18,18 +15,7 @@ fun ProfileScreen(
     Scaffold(
         topBar = {},
         content = {
-            DefaultButton(
-                modifier = Modifier,
-                text = "Cerrar sesion",
-                onClick = {
-                    viewModel.logout()
-                    navController.navigate(route = AppScreen.Login.route) {
-                        //ESTO ES PARA QUE UNA VEZ CERREMOS SESION BORRE ESTA PANTALLA
-                        // DE LA PILA DE PANTALLAS ANTERIORES
-                        popUpTo(AppScreen.Profile.route) { inclusive = true }
-                    }
-                }
-            )
+            ProfileContent(navController)
         },
         bottomBar = {}
     )
---
+++ app/src/main/java/com/saulo/ulpgcarapp/presentation/screens/profile/components/ProfileContent.kt
@@ -0,0 +1,98 @@
+package com.saulo.ulpgcarapp.presentation.screens.profile.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import com.saulo.ulpgcarapp.presentation.components.DefaultButton
+import com.saulo.ulpgcarapp.presentation.navigation.AppScreen
+import com.saulo.ulpgcarapp.presentation.screens.profile.ProfileViewModel
+import com.saulo.ulpgcarapp.R
+import com.saulo.ulpgcarapp.presentation.ui.theme.Orange400
+
+@Composable
+fun ProfileContent(navController: NavHostController, viewModel: ProfileViewModel = hiltViewModel()) {
+
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+
+        Box() {
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                painter = painterResource(id = R.drawable.carpooling),
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
+                alpha = 0.6f
+            )
+
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Spacer(modifier = Modifier.height(160.dp))
+                Image(
+                    modifier = Modifier.size(115.dp),
+                    painter = painterResource(id = R.drawable.user),
+                    contentDescription = ""
+                )
+                Spacer(modifier = Modifier.height(30.dp))
+                Text(text = "Bienvenido", fontSize = 30.sp, fontWeight = FontWeight.Bold)
+            }
+        }
+
+        Spacer(modifier = Modifier.height(55.dp))
+        Text(
+            text = "Nombre del usuario",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic
+        )
+
+        Text(
+            text = "Email del usuario",
+            fontSize = 15.sp,
+            fontStyle = FontStyle.Italic
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        DefaultButton(
+            modifier = Modifier.width(250.dp),
+            text = "Editar datos",
+            mainColor = Color.Black,
+            color = Orange400,
+            icon = Icons.Default.Edit,
+            onClick = { })
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        DefaultButton(
+            modifier = Modifier.width(250.dp),
+            text = "Cerrar sesion",
+            onClick = {
+                viewModel.logout()
+                navController.navigate(route = AppScreen.Login.route) {
+                    //ESTO ES PARA QUE UNA VEZ CERREMOS SESION BORRE ESTA PANTALLA
+                    // DE LA PILA DE PANTALLAS ANTERIORES
+                    popUpTo(AppScreen.Profile.route) { inclusive = true }
+                }
+            }
+        )
+
+    }
+}
